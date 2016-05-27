ba251295b0628e0fb3b72fe8216dd59ea53677ea
louisbuchbinder <louisbuchbinder@gmail.com>
2016-05-27 07:18:38 -0400

    added jquery on ready to allow page to load before script is run

--- client/dragndrop.js
+++ client/dragndrop.js
@@ -2,78 +2,80 @@
 const $ = require('jquery');
 
 const updateFiles = require('./files.js');
-// console.log(updateFiles);
 
-// files.push({name:'myFile', link:'/link'});
-// files.push({name:'myFile2', link:'/link2'});
+$(() => {
+  // console.log(updateFiles);
 
-
-// const addToFilesList = (name, link) => {
-//   var files = require('./files.js');
-//   files.push({name: name, link: link});
-// };
+  // files.push({name:'myFile', link:'/link'});
+  // files.push({name:'myFile2', link:'/link2'});
 
 
-// require('./files.js')('myFile', '/link');
+  // const addToFilesList = (name, link) => {
+  //   var files = require('./files.js');
+  //   files.push({name: name, link: link});
+  // };
+
+
+  // require('./files.js')('myFile', '/link');
 
 
 
-const allowDrop = (event) => event.preventDefault(); 
+  const allowDrop = (event) => event.preventDefault(); 
 
-// const drag = (event) => event.dataTransfer.setData('text', event.target.id);
+  // const drag = (event) => event.dataTransfer.setData('text', event.target.id);
 
-const sendFile = (file) => {
-  const xhr = new XMLHttpRequest();
-  xhr.open('POST', '/save', true);
-  xhr.setRequestHeader('filename', file.name);
-  xhr.send(file);
-
-
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState === 4 && xhr.status === 200) {
-      // var response = xhr.responseText.split(' /');
-      // var text = response[0];
-      // var link = '/' + response[1];
-      // $('#message').text(text);
-      // var $link = $('#link');
-      // $link.attr('href', link);
-      // $link.text(link);
-
-      updateFiles(file.name);
+  const sendFile = (file) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('POST', '/save', true);
+    xhr.setRequestHeader('filename', file.name);
+    xhr.send(file);
 
 
-      // require('./files.js')(file.name, link);
-      
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState === 4 && xhr.status === 200) {
+        // var response = xhr.responseText.split(' /');
+        // var text = response[0];
+        // var link = '/' + response[1];
+        // $('#message').text(text);
+        // var $link = $('#link');
+        // $link.attr('href', link);
+        // $link.text(link);
 
-      // files.push({name:'myFile', link:'/link'});
-      // files.push({name: name, link: link});
-      // console.log('pushed to files', files);
+        updateFiles(file.name);
 
-    }
+
+        // require('./files.js')(file.name, link);
+        
+
+        // files.push({name:'myFile', link:'/link'});
+        // files.push({name: name, link: link});
+        // console.log('pushed to files', files);
+
+      }
+    };
   };
-};
 
-const extractData = (event) => {};
+  const extractData = (event) => {};
 
-/*globals XMLHttpRequest*/
+  /*globals XMLHttpRequest*/
 
-const drop = (event) => {
-  event.preventDefault();
-  event.dataTransfer = event.originalEvent.dataTransfer;
-  // var data = event.dataTransfer.getData('text'); 
-  // console.log(data);
+  const drop = (event) => {
+    event.preventDefault();
+    event.dataTransfer = event.originalEvent.dataTransfer;
+    // var data = event.dataTransfer.getData('text'); 
+    // console.log(data);
 
-  var file = event.dataTransfer.files[0];
-
-
-  // console.log(file);
-  
-  sendFile(file);
-};
+    var file = event.dataTransfer.files[0];
 
 
+    // console.log(file);
+    
+    sendFile(file);
+  };
 
-const $dropbox = $('#dropbox');
 
-$dropbox.on({drop: drop, dragover: allowDrop});
 
+  const $dropbox = $('#dropbox');
+
+  $dropbox.on({drop: drop, dragover: allowDrop});
+});
