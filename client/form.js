d311d1e6c877a10e4850ee1ca2814482006ebd11
louisbuchbinder <louisbuchbinder@gmail.com>
2016-05-28 12:07:24 -0400

    redirect to home page on successful login to restart the app

--- client/form.js
+++ client/form.js
@@ -1,3 +1,5 @@
+/* globals location */
+
 const app = require('./app.js');
 
 
@@ -25,7 +27,7 @@ app.controller('loginController', function ($scope, $http) {
       url: '/login',
       data: {username: $scope.username, password: $scope.password}
     })
-    .then( function (results) { console.log(results); })
+    .then( function (results) { console.log(results); location.href = '/'; })
     .catch( function (error) { console.error(error); });
   };
   
