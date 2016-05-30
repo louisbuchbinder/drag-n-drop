05aee21a81b24b38016bd0fa5815c70f6af5b2d6
louisbuchbinder <louisbuchbinder@gmail.com>
2016-05-30 11:55:42 -0700

    completed task block drop across entire app

--- client/dragndrop.js
+++ client/dragndrop.js
@@ -6,6 +6,13 @@ const updateFiles = require('./files.js');
 
 //$(function () {
 
+
+
+
+//});
+const app = require('./app.js');
+app.controller('dropController', function ($scope, $sharedProps) {
+
   const allowDrop = function (event) { event.preventDefault(); };
 
   const sendFile = function (file) {
@@ -16,31 +23,28 @@ const updateFiles = require('./files.js');
 
     xhr.onreadystatechange = function() {
       if (xhr.readyState === 4 && xhr.status === 200) {
-        updateFiles(file.name);
+        // updateFiles(file.name);
+        $sharedProps.updateFiles[0](file.name);
       }
     };
   };
 
   const extractData = function (event) {};
 
+  const blockDrop = function (event) { event.preventDefault(); }; 
 
   const drop = function (event) {
     event.preventDefault();
     event.dataTransfer = event.originalEvent.dataTransfer;
-
     // currently only hardcoded for single files
     var file = event.dataTransfer.files[0];
-    
     sendFile(file);
   };
 
+  $('body').on({drop: blockDrop, dragover: allowDrop});
 
-
-//});
-  const app = require('./app.js');
-  app.controller('dropController', function ($scope, $sharedProps) {
-    const $dropbox = $('#dropbox');
-    $dropbox.on({drop: drop, dragover: allowDrop});    
-    $scope.loggedIn = $sharedProps.loggedIn;
-    $scope.username = $sharedProps.username;
-  });
+  const $dropbox = $('#dropbox');
+  $dropbox.on({drop: drop, dragover: allowDrop});    
+  $scope.loggedIn = $sharedProps.loggedIn;
+  $scope.username = $sharedProps.username;
+});
